dd5dbcf8f3fccabba0822d786bda1058d3cc8557
Ariful Islam <ariful6@yahoo.com>
2025-01-21 23:12:12 +0200

    c0ex02

--- c02.c
+++ c02.c
@@ -1,4 +1,4 @@
-// C0100 Reproduce the behavior of the function strcpy (man strcpy)
+// C0100 Reproduce the behavior of the function strcpy
 char	*ft_strcpy(char *dest, char *src) {
 	int	i = 0;
 	while (src[i])
@@ -9,4 +9,17 @@ char	*ft_strcpy(char *dest, char *src) {
 	dest[i] = src[i];
 	return (dest);
 }
+char	*ft_strcpy(char *dest, char *src) {
+	char *original_destination = dest;
 
+	while (*src != '\0') {
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return original_destination;
+}
+
+// Reproduce the behavior of the function strncpy 
